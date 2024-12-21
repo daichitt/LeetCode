68e1fbcfcc94c7af4064fee36de4adc461824734
daichit <gene.sixteen.000925@outlook.jp>
2024-12-21 11:33:39 +0700

    FIx isValid

--- Python/Stack/isValid.py
+++ Python/Stack/isValid.py
@@ -4,22 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        # use hash map (dict) # time O(n)
+        if len(s) % 2 != 0: return False 
+        # Stack and mapping of brackets
         stack = []
+        brackets = {"(": ")", "[": "]", "{": "}"}
 
-        brackets = {
-            '(' : ')', 
-            '{' : '}',
-            '[' : ']', 
-        }
-
-        for braket in s:
-            if braket in brackets.keys(): # key  such as '('
-                stack.append(braket)
-            elif braket in brackets.values():
-                if not stack or brackets[stack.pop()] != braket:
+        for char in s:
+            if char in brackets:  # If it's an opening bracket
+                stack.append(char)
+            else:  # If it's a closing bracket
+                if not stack or brackets[stack.pop()] != char:
                     return False
-        
-        return len(stack) == 0
 
-        # check keys() and values()
+        # Check if all brackets are matched
+        return not stack
